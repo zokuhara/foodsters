e1a42c337606e9c50e3417b5b7afcde1610fc682
Zachary Okuhara <zokuhara@gmail.com>
2015-11-11 19:48:04 -0600

    Re-Seed db

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20151109155010) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-
-  create_table "recipes", force: :cascade do |t|
-    t.string   "summary"
-    t.text     "description"
-    t.date     "dtstart"
-    t.string   "ip_class",    default: "PUBLIC"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-  end
 
   create_table "payola_affiliates", force: :cascade do |t|
     t.string   "code"
@@ -126,15 +114,6 @@ ActiveRecord::Schema.define(version: 20151109155010) do
 
   add_index "payola_subscriptions", ["guid"], name: "index_payola_subscriptions_on_guid", using: :btree
 
-  create_table "subscriptions", force: :cascade do |t|
-    t.integer  "amount"
-    t.string   "interval"
-    t.string   "stripe_id"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "recipes", force: :cascade do |t|
     t.string   "summary"
     t.text     "description"
@@ -142,6 +121,15 @@ ActiveRecord::Schema.define(version: 20151109155010) do
     t.string   "ip_class",    default: "PUBLIC"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "amount"
+    t.string   "interval"
+    t.string   "stripe_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
